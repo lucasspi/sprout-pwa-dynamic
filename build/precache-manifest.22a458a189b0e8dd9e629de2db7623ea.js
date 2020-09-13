43cc38c72da0c7dc0ae290b2198ebefa931bd9d1
Lucas Spirandeli <lucasspi@outlook.com>
2020-09-13 10:09:19 -0300

    Initial Commit

---
+++ build/precache-manifest.22a458a189b0e8dd9e629de2db7623ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5eb490f0636c0dc1f4cf7966c8d267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e202c08b0538ea0939a",
+    "url": "/static/css/main.3c73931e.chunk.css"
+  },
+  {
+    "revision": "fca7b7f00516c389716d",
+    "url": "/static/js/2.0d79d862.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0d79d862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e202c08b0538ea0939a",
+    "url": "/static/js/main.e1c2eb08.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  }
+]);
